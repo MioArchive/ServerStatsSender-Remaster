99a0f11786cd91d054795d663e98212a08e86395
MLGPenguin <baww123@gmail.com>
2024-10-27 13:28:42 +1000

    Optimise build.gradle and build

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,51 +5,42 @@ plugins {
 }
 
 group = "com.lent"
-version = "1.0.1"
+version = "1.0.2"
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/") {
-        name = "spigotmc-repo"
-    }
-    maven("https://oss.sonatype.org/content/groups/public/") {
-        name = "sonatype"
-    }
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     compileOnly("org.spigotmc:spigot-api:1.20.4-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("net.dv8tion:JDA:5.1.2") { // replace $version with the latest version
+
+    compileOnly("net.dv8tion:JDA:5.1.2") {
     compileOnly("me.lucko:spark-api:0.1-SNAPSHOT")
-
-
     }
 }
-val targetJavaVersion = 17
+
 kotlin {
-    jvmToolchain(targetJavaVersion)
+    jvmToolchain(17)
 }
 
 tasks {
+    build {
+        dependsOn("shadowJar")
+    }
+
     shadowJar {
         minimize() // Use minimization
-        destinationDirectory.set(file("output/dir"))
-        exclude("me.lucko:spark-api:0.1-SNAPSHOT")
-        exclude("net.dv8tion:JDA:5.1.2")
     }
-}
 
-tasks.build {
-    dependsOn("shadowJar")
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filteringCharset = "UTF-8"
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
     }
 }
